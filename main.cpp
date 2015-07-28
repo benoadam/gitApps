c97093658f329fca91a77fe4feaf75f3eaedd14e
Adam Beňo <adam.beno@szm.com>
2015-07-28 08:51:13 +0200

    prva skuska

--- main.cpp
+++ main.cpp
@@ -1,8 +1,9 @@
 #include <QCoreApplication>
-
+#include <iostream>
+using namespace std;
 int main(int argc, char *argv[])
 {
     QCoreApplication a(argc, argv);
-
+    cout<<"prva zmena"<<endl;
     return a.exec();
 }
